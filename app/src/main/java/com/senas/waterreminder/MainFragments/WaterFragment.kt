a48135e1751f4d8fa8e926e4ab4448df2ca2eae1
Sena Solmaz <59575118+senasolmaz@users.noreply.github.com>
2020-03-02 22:12:30 +0300

    Water Fragment finish

--- app/src/main/java/com/senas/waterreminder/MainFragments/WaterFragment.kt
+++ app/src/main/java/com/senas/waterreminder/MainFragments/WaterFragment.kt
@@ -1,10 +1,16 @@
 package com.senas.waterreminder.MainFragments
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.transition.Slide
+import android.util.Log
+import android.view.*
 import android.widget.*
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
@@ -12,80 +18,95 @@ import com.senas.waterreminder.R
 import kotlin.math.roundToInt
 import kotlin.system.exitProcess
 
-class WaterFragment: Fragment() {
+class WaterFragment : Fragment() {
 
     private lateinit var viewPager: ViewPager
     private lateinit var tabLayout: TabLayout
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_water, container, false)
 
         val seekBar = view.findViewById<SeekBar>(R.id.seekBar)
         val textView = view.findViewById<TextView>(R.id.txtView)
+        val bugunToplam = view.findViewById<TextView>(R.id.bugun_toplam)
         val view2 = view.findViewById<View>(R.id.viewHeight)
         val glassImage = view.findViewById<ImageView>(R.id.glases)
         val plusButon = view.findViewById<ImageButton>(R.id.plus)
         val minusButon = view.findViewById<ImageButton>(R.id.minus)
-        val confirmButon = view.findViewById<ImageButton>(R.id.confirmButon)
+        val confirmButon = view.findViewById<Button>(R.id.confirmButon)
 
-        var defaultML = 20;
+        var defaultML = 100;
         var icilenToplamMiktar = 0;
+        confirmButon.isVisible = false
 
         val density = resources.displayMetrics.density
         val imageParams = glassImage.layoutParams
-        imageParams.width = (140*density).toInt()
-        imageParams.height = (200*density).toInt()
-
+        imageParams.width = (130 * density).toInt()
+        imageParams.height = (190 * density).toInt()
         val layoutParams = view2.layoutParams
 
         seekBar?.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seek: SeekBar,
-                                           progress: Int, fromUser: Boolean) {
-
-                textView.text = progress.toString()
-
-                if(progress*2.2 < 210 ) {
-                    val layoutParams = view2.layoutParams
-                    layoutParams.height = (progress*2.2*density).toInt()
-                    view2.layoutParams = layoutParams
-                }
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+                textView.text = icilenToplamMiktar.toString() + "/3000 ML"
             }
-
             override fun onStartTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is started
             }
-
             override fun onStopTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is stopped
-                //    textView.text = "Progress is: " + seek.progress + "%"
             }
         })
 
+        seekBar.setOnTouchListener(View.OnTouchListener { view, motionEvent -> true })
+
         plusButon.setOnClickListener {
-            if(icilenToplamMiktar < 210 ) {
-                layoutParams.height += defaultML*2
+            if (layoutParams.height < 190*density && icilenToplamMiktar < 3000) {
+                layoutParams.height += (defaultML * 190 / 3000*density).toInt()
                 icilenToplamMiktar += defaultML
                 view2.layoutParams = layoutParams
-            }
-            seekBar.progress = (icilenToplamMiktar*0.2).roundToInt()
+           }
+            seekBar.progress = icilenToplamMiktar / defaultML * 3
+            confirmButon.isVisible = true
+            Log.d("top", icilenToplamMiktar.toString())
         }
         minusButon.setOnClickListener {
-            if(icilenToplamMiktar < 210 && icilenToplamMiktar > 0) {
-                layoutParams.height -= defaultML*2
+            if (icilenToplamMiktar < 3001 && icilenToplamMiktar > 0) {
+                layoutParams.height -=   (defaultML * 190 / 3000*density).toInt()
                 icilenToplamMiktar -= defaultML
                 view2.layoutParams = layoutParams
+            } else {
+                confirmButon.isVisible = false
             }
-             seekBar.progress = (icilenToplamMiktar*0.2).roundToInt()
+            seekBar.progress = icilenToplamMiktar / defaultML * 3
         }
 
-        confirmButon.setOnClickListener{
+        confirmButon.setOnClickListener {
 
-           // seekBar.progress = icilenToplamMiktar
-            Toast.makeText(context, icilenToplamMiktar.toString(), Toast.LENGTH_SHORT).show()
+            // seekBar.progress = icilenToplamMiktar
+            //Toast.makeText(context, icilenToplamMiktar.toString(), Toast.LENGTH_SHORT).show()
+            val dialog = Dialog(context!!)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.water_added_popup)
+
+            //    val infoText = dialog .findViewById(R.id.bilgiText) as TextView
+            val okBtn = dialog.findViewById(R.id.okButon) as Button
+            okBtn.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.show()
+
+            bugunToplam.text = "Bugün " + icilenToplamMiktar.toString() + " / 3000 ML"
+
         }
+        bugunToplam.text = "Bugün " + icilenToplamMiktar.toString() + " / 3000 ML"
 
         return view
     }
